d02a7bc7c501ad8190935bcdd1055593b242bb36
Barb Banbury <darwinthesun@gmail.com>
2014-07-07 12:56:37 -0700

    added rOpenTree.R

---
+++ rOpenTree.R
@@ -0,0 +1,35 @@
+Hackathon ideas and personal goals *
+The hackathon is a participant-driven event, dependent on your ideas and goals. Please describe what motivates you to participate in the hackathon. Briefly describe a project to leverage OpenTree resources that would interest you, and explain very briefly the role you might play in that project.
+
+
+I am excited to apply to be part of the hackathon, as it overlaps with quite a few of my research interests. I am an evolutionary biologist who uses trees and data to understand biodiversity.  My research is almost exclusively in R, so my specific interest in OpenTree is massaging data from the OpenTree API into my R workflow. 
+
+I think an R package that bridges the R environment and the OpenTree API would be useful to many different fields of biology and would "fill the gap".  There are already a bunch of other API driven R packages and we should make sure to cross communicate with these.  
+
+Key deliverables should be an R formatted tree (usable by Ape), way to get tree subsets via api or ape, ottids from api, trees by study, metadata. 
+
+
+In general, I am interested in getting OpenTree data into R so that I can extract out clades of interest and run comparative analyses. Ultimately, I would like to have an R package (or maybe scripts to be incorporated with Arbor) that bridges the OpenTree API and allows for downloading a particular subset of the tree. For example, a user could pass a taxon string to the API and get back a tree with just those taxa. These API driven functions can include different kinds of 
+
+I am interested in extracting information from OpenTree and porting to R. This "information" could come in many forms, from tree topology, branch lengths, metadata.  Once in R, these information can be used to create subtrees that contain certain information, for example only certain species or from a particular source.  
+
+
+
+Knowledge, skills & experience *
+Please describe concisely the knowledge, skills and experiences that prepare you to participate successfully in the planned hackathon. Use general statements to show the big picture, and also provide concrete examples (e.g., code or docs written, analyses performed, papers published). Please allow us a chance to see your skills and knowledge by citing outcomes that we can verify (e.g., via citation, URL).
+
+I am a phylogeneticist who uses trees and comparative data to understand evolutionary processes. I am also an experienced R programmer, including package development (phrynomics [Banbury & Leache], Reol [Banbury & O'Meara], rPlant [Banbury et al.], TreEvo [O’Meara and Banbury]). I have experience writing functions that interact with other publicly funded APIs, including the Encyclopedia of life (Reol), iPlant, CIPRES, and the Taxonomic Name Resolution Service (rPlant). And I am involved in the broader R community by contributing to rOpenSci, being a Google Summer of Code Mentor, and local R Meetups.  
+
+Banbury B.L. and B.C. O’Meara. 2014. Reol: R interface to the Encyclopedia of Life. Ecology and Evolution 4(12):2577–2583.
+Banbury B.L. and Adam Leaché. phrynomics. Subversion repository available at https://github.com/bbanbury/phrynomics, and our shiny app at https://rstudio.stat.washington.edu/shiny/phrynomics/. 
+Banbury B.L. and B.C. O’Meara. Reol. Available on CRAN (http://cran.r-project.org/web/packages/Reol/).  Subversion repository available at https://r-forge.r-project.org/projects/reol/.
+Banbury B.L., Michels K., Beaulieu J., and B.C. O’Meara. rPlant. Available on CRAN (http://cran.r-project.org/web/packages/rPlant/index.html).  Subversion repository available at https://r-forge.r-project.org/projects/rplant/.
+O’Meara B.C and B.L Banbury. TreEvo. Subversion repository available at https://r-forge.r-project.org/projects/treevo/. 
+
+
+
+
+
+
+
+
